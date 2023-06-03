d14bae02f811e4d6773019485b17556200a138b0
Moivvas <130219729+Moivvas@users.noreply.github.com>
2023-06-03 17:41:44 +0300

    def main

--- sort.py
+++ sort.py
@@ -19,6 +19,7 @@ print("Тека:", folder_path)
 TRANS = {1072: 'a', 1040: 'A', 1073: 'b', 1041: 'B', 1074: 'v', 1042: 'V', 1075: 'g', 1043: 'G', 1076: 'd', 1044: 'D', 1077: 'e', 1045: 'E', 1105: 'e', 1025: 'E', 1078: 'j', 1046: 'J', 1079: 'z', 1047: 'Z', 1080: 'i', 1048: 'I', 1081: 'j', 1049: 'J', 1082: 'k', 1050: 'K', 1083: 'l', 1051: 'L', 1084: 'm', 1052: 'M', 1085: 'n', 1053: 'N', 1086: 'o', 1054: 'O', 1087: 'p', 1055: 'P', 1088: 'r', 1056: 'R', 1089: 's', 1057: 'S', 1090: 't', 1058: 'T', 1091: 'u', 1059: 'U', 1092: 'f', 1060: 'F', 1093: 'h', 1061: 'H', 1094: 'ts', 1062: 'TS', 1095: 'ch', 1063: 'CH', 1096: 'sh', 1064: 'SH', 1097: 'sch', 1065: 'SCH', 1098: '', 1066: '', 1099: 'y', 1067: 'Y', 1100: '', 1068: '', 1101: 'e', 1069: 'E', 1102: 'yu', 1070: 'YU', 1103: 'ya', 1071: 'YA', 1108: 'je', 1028: 'JE', 1110: 'i', 1030: 'I', 1111: 'ji', 1031: 'JI', 1169: 'g', 1168: 'G'}
 
 def normalize(name):
+
     translated = name.translate(TRANS)
     normalize_name = ''
     for i in translated:
@@ -44,6 +45,7 @@ for folder_name in extensions.keys():
 
 # Для повторююмих імен файлів
 def get_new_path(folder, item):
+
     new_name = normalize(item.stem)
     extension = item.suffix
     new_path = folder / (new_name + extension)
@@ -60,6 +62,7 @@ def get_new_path(folder, item):
 
 # Сортувальник файлів з рекурсією 
 def sorting_all(folder):
+
     for item in folder.iterdir():
         if item.is_file():
             extension = item.suffix.upper()[1:]
@@ -76,11 +79,9 @@ def sorting_all(folder):
         elif item.is_dir():
             sorting_all(item)
 
-
-sorting_all(folder_path)
-
 # розпаковка архівів
 def unpack_archive(archive):
+
     archive_folder = archive / 'archives'
     for arch in archive_folder.iterdir():
         if arch.is_dir():
@@ -92,10 +93,9 @@ def unpack_archive(archive):
             shutil.unpack_archive(str(arch), unpack_archive_folder)
             os.remove(arch)
 
-unpack_archive(folder_path)
-
 # видалення пустих папок
 def clean(sort_path):
+
     for item in sort_path.iterdir():
         if item.is_dir():
             clean(item)
@@ -103,4 +103,12 @@ def clean(sort_path):
                 item.rmdir()
         elif item.is_file():
             continue
-clean(folder_path)
+
+def main():
+        
+    sorting_all(folder_path)
+    unpack_archive(folder_path)
+    clean(folder_path)
+
+if __name__ == '__main__':
+    main()
